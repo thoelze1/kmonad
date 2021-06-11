98ac472e7eb2348713b7b647f9d092811388bd16
Tanner Hoelzel <thoelze1@binghamton.edu>
2021-06-11 14:42:55 +0100

    employ state monad

--- attempt3.hs
+++ attempt3.hs
@@ -1,11 +1,16 @@
+import Control.Monad.State
+
+-- Add a monad as a type parameter to replace the "Maybe" below.
 data Layer a c = Layer
-  { ctx  :: c
-  , die  :: (a,c) -> Bool
-  , emit :: (a,c) -> [a]
-  , gen  :: (a,c) -> [Layer a c]
-  , ctx' :: (a,c) -> c
+  { ctx :: c
+  , run :: a -> State (Maybe c) ([a],[Layer a c])
   -- IO goes here
   }
+
+type Manip a = a -> State (Maybe [a]) ([a],[Layer a [a]])
+
+mkLayer :: Manip a -> Layer a [a]
+mkLayer = Layer []
 
 instance Show c => Show (Layer a c) where
   show = show . ctx
@@ -17,85 +22,87 @@ data Tree a c
 
 instance Show c => Show (Tree a c) where
   show Empty = ""
-  show (Tree l t) = show l ++ "\n" ++ show t
-
-type T a = Tree a [a]
-
-updateCtx :: Layer a c -> a -> Layer a c
-updateCtx (Layer c d e g c') x =
-  Layer (c' (x,c)) d e g c'
+  show (Tree l t) = (show l) ++ "\n" ++ (show t)
 
 insert :: [Layer a c] -> Tree a c -> Tree a c
 insert ls t = foldr Tree t ls
 
+type T a = Tree a [a]
+
 send :: T a -> a -> T a
 send Empty _ = Empty
-send (Tree l t) x = if die l v
-  then t
-  else Tree l' t''
+send (Tree l t) x =
+  case s of
+    Nothing -> t
+    Just c' -> Tree (Layer c' r) t'
   where
-    v = (x,ctx l)
-    l' = updateCtx l x
-    t' = insert (gen l v) t
-    t'' = foldl send t' (emit l v)
+    r = run l
+    ((xs,ls),s) = runState (r x) (Just $ ctx l)
+    t' = foldl send (insert ls t) xs
 
-printIdTree :: IO ()
-printIdTree = do
-  print t'
-    where
-      live = const False
-      null = const []
-      out = Layer [] live null null (\(x,c) -> x:c)
-      clear = Layer [] live (\(x,_) -> [x]) null null
-      t = Tree clear (Tree out Empty)
-      t' = foldl send t ['a','b','c']
+exec :: T a -> [a] -> T a
+exec = foldl send
 
-printMapTree :: IO ()
-printMapTree = do
-  print t'
-    where
-      live = const False
-      null = const []
-      out = Layer [] live null null (\(x,c) -> x:c)
-      remap :: (Char,[Char]) -> [Char]
-      remap ('a',_) = ['b']
-      remap (c,_) = [c]
-      dummy = Layer [] live remap null null
-      t = Tree dummy (Tree out Empty)
-      t' = foldl send t ['a','b','c']
+-- FIXME Because we are invoking the following stateful transofmations
+-- from within `send`, it is impossible that the result of `<- get`
+-- will ever be of the form `Nothing` (based on the behavior of
+-- `send`). Therefore I think using a Maybe context for the state to
+-- communicate to `send` whether the layer should be removed or not is
+-- a hack.
 
-printModTree :: IO ()
-printModTree = do
-  print t'
-    where
-      live = const False
-      null = const []
-      out = Layer [] live null null (\(x,c) -> x:c)
-      remap :: (Char,[Char]) -> [Char]
-      remap ('a',_) = ['b']
-      remap (c,_) = [c]
-      trigger = Layer
-                []
-                live
-                (\(c,l) -> if c == 'x'
-                  then []
-                  else [c])
-                (\(c,l) -> if c == 'x'
-                  then [Layer
-                       []
-                       ((==) 'y' . fst)
-                       remap
-                       null
-                       null]
-                  else [])                                            
-                null
-      t = Tree trigger (Tree out Empty)
-      t' = foldl send t ['a','b','c','x','a','a','a','a','y','x','y']
+getMatches :: (Eq a) => [a] -> [b] -> a -> [b]
+getMatches xs ys x = snd $ unzip $ filter ((==) x . fst) (zip xs ys)
 
--- if it dies, it doesn't need to return context... so we could return
--- a Maybe Context instead of explicitly returning a Bool. In this
--- model, Nothing would represent removing the layer and Just x would
--- represent updating context
+-- We currently have remap written in terms of listRemap. But is there
+-- a way to write listRemap in terms of remap? Perhaps such a way
+-- would hint at better ways of combining multiple layers...
 
--- We can replace the above code with state monad! instead of
--- explcitly tracking and passing context!
+listRemap :: (Eq a) => [a] -> [a] -> Manip a
+listRemap xs ys c
+  | c `elem` xs = do
+      return (getMatches xs ys c,[])
+  | otherwise = return ([c],[])
+
+remap :: (Eq a) => a -> a -> Manip a
+remap x y = listRemap [x] [y]
+
+listRemapWithKill :: (Eq a) => [a] -> [a] -> a -> Manip a
+listRemapWithKill xs ys k c
+  | c == k = do
+      put Nothing
+      return ([],[])
+  | otherwise = listRemap xs ys c
+
+modTrigger :: (Eq a) => a -> Manip a -> Manip a
+modTrigger x m c
+  | c == x = do
+      return ([],[mkLayer m])
+  | otherwise = do
+      return ([c],[])
+
+out :: Manip a
+out c = do
+  mc <- get
+  case mc of
+    Nothing -> return ([],[])
+    Just cs -> do
+      put (Just (c:cs))
+      return ([],[])
+
+clear :: Manip a
+clear c = do
+  return ([c],[])
+
+atob :: Manip Char
+atob = listRemap ['a'] ['b']
+
+{-
+atob :: Manip Char
+atob c = do
+  case c of
+    'a' -> return (['b'],[])
+    _ -> return ([c],[])
+-}
+
+modatob :: Manip Char
+modatob = modTrigger 'x' (listRemapWithKill ['a'] ['b'] 'y')
